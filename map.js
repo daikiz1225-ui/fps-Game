d7cc185e40fd874f1f5c7d2ac1628cd869d00425
daikiz1225-ui <daikiz1225@icloud.com>
2026-02-25 11:33:59 +0900

    Update map.js

--- map.js
+++ map.js
@@ -12,7 +12,6 @@ function createVoxelWall(width, height, depth, x, y, z, color, isFloor = false) 
                     new THREE.BoxGeometry(isFloor ? 1.9 : 0.95, isFloor ? 0.4 : 0.95, isFloor ? 1.9 : 0.95),
                     new THREE.MeshStandardMaterial({ color: color })
                 );
-                // 配置間隔の調整
                 const px = x + (ix - (width - 1) / 2) * (isFloor ? 2 : 1);
                 const py = y + (iy - (height - 1) / 2) * (isFloor ? 0.4 : 1);
                 const pz = z + (iz - (depth - 1) / 2) * (isFloor ? 2 : 1);
@@ -22,7 +21,6 @@ function createVoxelWall(width, height, depth, x, y, z, color, isFloor = false) 
             }
         }
     }
-    // キャラクターの当たり判定
     if (!isFloor) {
         colliders.push({ 
             pos: new THREE.Vector3(x, y, z), 
@@ -34,22 +32,12 @@ function createVoxelWall(width, height, depth, x, y, z, color, isFloor = false) 
 }
 
 export function createMap() {
-    // 地面（タイルブロック）
     createVoxelWall(40, 1, 40, 0, -0.2, 0, 0x888888, true);
-
-    // 1Pリスポーン (青)
     createVoxelWall(10, 1, 10, 50, 0.2, 0, 0x0000ff);
-    // 2Pリスポーン (青)
     createVoxelWall(10, 1, 10, -50, 0.2, 0, 0x0000ff);
-
-    // 中央：緑の超高台
     createVoxelWall(10, 20, 10, 0, 10, 0, 0x00ff00);
-
-    // 青：L字の壁1
     createVoxelWall(10, 8, 2, 25, 4, 30, 0x0000ff);
     createVoxelWall(2, 8, 10, 31, 4, 26, 0x0000ff);
-
-    // 青：L字の壁2
     createVoxelWall(10, 8, 2, -25, 4, -30, 0x0000ff);
     createVoxelWall(2, 8, 10, -31, 4, -26, 0x0000ff);
 }
