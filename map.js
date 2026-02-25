1969d7600f568641a12e1f1605a76ddb71535637
daikiz1225-ui <daikiz1225@icloud.com>
2026-02-25 11:40:33 +0900

    Update map.js

--- map.js
+++ map.js
@@ -4,7 +4,8 @@ import { scene } from './scene.js';
 export const colliders = [];
 export const paintableBlocks = [];
 
-function createVoxelWall(width, height, depth, x, y, z, color, isFloor = false) {
+// ブロックを生成する関数
+function createVoxel(width, height, depth, x, y, z, color, isFloor = false) {
     for (let ix = 0; ix < width; ix++) {
         for (let iy = 0; iy < height; iy++) {
             for (let iz = 0; iz < depth; iz++) {
@@ -22,22 +23,19 @@ function createVoxelWall(width, height, depth, x, y, z, color, isFloor = false) 
         }
     }
     if (!isFloor) {
-        colliders.push({ 
-            pos: new THREE.Vector3(x, y, z), 
-            h: height, 
-            sizeW: width / 2, 
-            sizeD: depth / 2 
-        });
+        colliders.push({ pos: new THREE.Vector3(x, y, z), h: height, sizeW: width / 2, sizeD: depth / 2 });
     }
 }
 
 export function createMap() {
-    createVoxelWall(40, 1, 40, 0, -0.2, 0, 0x888888, true);
-    createVoxelWall(10, 1, 10, 50, 0.2, 0, 0x0000ff);
-    createVoxelWall(10, 1, 10, -50, 0.2, 0, 0x0000ff);
-    createVoxelWall(10, 20, 10, 0, 10, 0, 0x00ff00);
-    createVoxelWall(10, 8, 2, 25, 4, 30, 0x0000ff);
-    createVoxelWall(2, 8, 10, 31, 4, 26, 0x0000ff);
-    createVoxelWall(10, 8, 2, -25, 4, -30, 0x0000ff);
-    createVoxelWall(2, 8, 10, -31, 4, -26, 0x0000ff);
+    // 地面（灰色）
+    createVoxel(40, 1, 40, 0, -0.2, 0, 0x888888, true);
+    // リスポーン（青）
+    createVoxel(10, 1, 10, 50, 0.2, 0, 0x0000ff);
+    createVoxel(10, 1, 10, -50, 0.2, 0, 0x0000ff);
+    // 中央高台（緑）
+    createVoxel(10, 20, 10, 0, 10, 0, 0x00ff00);
+    // L字の壁（青）
+    createVoxel(10, 8, 2, 25, 4, 30, 0x0000ff);
+    createVoxel(2, 8, 10, 31, 4, 26, 0x0000ff);
 }
